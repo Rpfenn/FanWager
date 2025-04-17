53b6b69aa247161eeb23ce1a991c3bc3352e45a4
Rpfenn <rpfennelly04@gmail.com>
2025-04-17 01:28:43 -0400

    Added NavBar

--- app/src/main/java/edu/quinnipiac/ser210/finalproject/screens/HistoryScreen.kt
+++ app/src/main/java/edu/quinnipiac/ser210/finalproject/screens/HistoryScreen.kt
@@ -1,2 +1,27 @@
 package edu.quinnipiac.ser210.finalproject.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HistoryScreen() {
+    Box(
+        modifier =
+        Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Text(
+            text = "History Screen",
+
+            fontFamily = FontFamily.Cursive,
+            fontSize = 22.sp
+        )
+    }
+}
